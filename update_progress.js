e4432ead897f9a44c0583015a1476146a2c3659a
ClooverDev <130509634+ClooverDev@users.noreply.github.com>
2024-10-26 19:00:22 -0300

    Update update_progress.js

--- update_progress.js
+++ update_progress.js
@@ -1,6 +1,6 @@
 // PROGRESS BAR //
-const progress_current = 0;
-const current_part_being_produced = "UNKOWN PROJECT - BASE GAME (HALLOWEEN PROJECT)";
+const progress_current = 25;
+const current_part_being_produced = "UNKOWN PROJECT - BASE GAME";
 
 function update_progress_bar() {
 const perc = document.getElementById("pb_percentage");
