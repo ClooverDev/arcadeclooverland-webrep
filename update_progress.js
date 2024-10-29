84453616239800a1a9db7f4f46e42a46414c5be7
ClooverDev <130509634+ClooverDev@users.noreply.github.com>
2024-10-29 12:57:33 -0300

    Update update_progress.js

--- update_progress.js
+++ update_progress.js
@@ -1,5 +1,5 @@
 // PROGRESS BAR //
-const progress_current = 75;
+const progress_current = 89;
 const current_part_being_produced = "SUPER PLATFORM: REORIGINAL - BASE GAME";
 
 function update_progress_bar() {
