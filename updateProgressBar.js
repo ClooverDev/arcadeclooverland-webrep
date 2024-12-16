4e764454ac4db14cd86dc881b004bec07fb9c859
ClooverDev <130509634+ClooverDev@users.noreply.github.com>
2024-12-15 21:08:42 -0300

    rework

---
+++ updateProgressBar.js
@@ -0,0 +1,10 @@
+function update_progress_bar() {
+	const inner_bar = "#bar_inner";
+	const bar_title = document.getElementById("bar_title");
+	
+	const CURRENT_PROGRESS_INT = 75;
+	const CURRENT_PROGRESS_TITLE = "SUPER PLATFORM: R | CHRISTMAS & PART 2";
+	
+	$(inner_bar).width(CURRENT_PROGRESS_INT+"%");
+	bar_title.innerHTML = CURRENT_PROGRESS_TITLE+" <br><span style='color:white; font-size: 32px;'>["+CURRENT_PROGRESS_INT+"%]</span>";
+}
