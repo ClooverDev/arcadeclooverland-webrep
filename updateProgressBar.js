c12175029fe5ebcfe902a5ff0ae3235f75073eaf
ClooverDev <130509634+ClooverDev@users.noreply.github.com>
2024-12-28 01:38:45 -0300

    Update updateProgressBar.js

--- updateProgressBar.js
+++ updateProgressBar.js
@@ -3,7 +3,7 @@ function update_progress_bar() {
 	const bar_title = document.getElementById("bar_title");
 	
 	const CURRENT_PROGRESS_INT = 0;
-	const CURRENT_PROGRESS_TITLE = "SUPER PLATFORM: R | LITTLE CHRISTMAS";
+	const CURRENT_PROGRESS_TITLE = "SUPER PLATFORM: R | HUNT OR HUNTED UPDATE";
 	
 	$(inner_bar).width(CURRENT_PROGRESS_INT+"%");
 	bar_title.innerHTML = CURRENT_PROGRESS_TITLE+" <br><span style='color:white; font-size: 32px;'>["+CURRENT_PROGRESS_INT+"%]</span>";
